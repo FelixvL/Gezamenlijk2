92c42cca9e54139f36fa266132804da74e8a11ad
sgehin <sfa.gehin@gmail.com>
2017-11-15 16:03:47 +0100

    emergency on master

--- index.php
+++ index.php
@@ -6,16 +6,13 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-
-
         <link rel = "stylesheet" type = "text/css" href="SportPool.css">
         <link rel = "stylesheet" type = "text/css" href="sportpoolCSS.css">
-
     </head>
 
     <body>
-        
-         <img src="voetbal.jpg" >
+
+        <img src="voetbal.jpg" >
 
         <!--<a href="teams.php">maak teams aan</a>-->
         <form action="teams.php" method="get">
@@ -27,32 +24,44 @@
 
         <?php
         $conn = connectionDB();
-        echo createTagSelect($conn);
-        echo createTagSelect($conn);
+        echo createTagSelect($conn, "id1");
+        echo createTagSelect($conn, "id2");
         ?>
+        <script>
+            function checkdouble() {
+                x = document.getElementById("id1").selectedIndex;
+                y = document.getElementById("id2").selectedIndex;
+                if (x === y)
+                {
+                    alert("Please choose different teams");
+                }
+
+            }
+
+        </script>
     <center>
         <div style="overflow-x:auto; padding-top: 100px">
             <table style="border: 2px">
-        <?php
-        $sql = "SELECT * FROM `elftal`;";
-        $result = $conn->query($sql);
-        echo "<tr>";
-        for ($x = 0; $x < $result->num_rows; $x++) {
-            $row = $result->fetch_assoc();
-            echo "<tr>";
-            echo "<td>";
-            echo $row['id'];
-            echo "</td>";
-            echo "<td>";
-            echo $row['naam'];
-            echo "</td>";
-            echo "<td>";
-            echo $row['plaats'];
-            echo "</td>";
-            echo "</tr>";
-        }
-        echo "</tr>";
-        ?>  
+                <?php
+                $sql = "SELECT * FROM `elftal`;";
+                $result = $conn->query($sql);
+                echo "<tr>";
+                for ($x = 0; $x < $result->num_rows; $x++) {
+                    $row = $result->fetch_assoc();
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $row['id'];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $row['naam'];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $row['plaats'];
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                echo "</tr>";
+                ?>  
             </table>
         </div>
     </center>
@@ -62,7 +71,7 @@
 
 
 
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
 
