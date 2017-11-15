f31d73fcc053d24084ba5d38735e69f5773bc2b7
sgehin <sfa.gehin@gmail.com>
2017-11-15 14:28:00 +0100

    including PHP files and fixing footer

--- index.php
+++ index.php
@@ -1,50 +1,44 @@
+<?php include 'generalfunctions.php'; ?>
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
-        
-        
+
+
         <link rel = "stylesheet" type = "text/css" href="SportPool.css">
-       
+
     </head>
-   
+
     <body>
 
         <!--<a href="teams.php">maak teams aan</a>-->
         <form action="teams.php" method="get">
             <button type=submit value="teams"  >  teams aanmaken </button>
         </form>
-            
-            
-        
+
+
+
         <br> kleine verbetering aa
 
 
-<?php
-
-    $hostname='localhost';
-    $databasenaam='sport_pool';
-    $username='root';
-    $password='';
-    
-    $conn = new mysqli($hostname, $username, $password, $databasenaam);
-    $sql = "SELECT * FROM `elftal`;";
-    $result = $conn->query($sql);
-    echo "<select>";
-    for($x = 0; $x < $result->num_rows; $x++){
-        $row = $result->fetch_assoc();
-        echo "<option>";
-        echo $row['naam'];
-        echo "</option>";
-    }
-    echo "</select>";
-        
-
-?>
+        <?php
+        connectionDB();
+        $sql = "SELECT * FROM `elftal`;";
+        $result = $conn->query($sql);
+        echo "<select>";
+        for ($x = 0; $x < $result->num_rows; $x++) {
+            $row = $result->fetch_assoc();
+            echo "<option>";
+            echo $row['naam'];
+            echo "</option>";
+        }
+        echo "</select>";
+        ?>
         <br>
-       
 
 
-        <?php include 'footer.php'; ?>
+
+<?php include 'footer.php'; ?>
     </body>
 </html>
