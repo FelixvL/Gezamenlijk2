4e90d08e0c344450fbef090eeb2b9e80ddb137d3
TJKnigge <tjknigge1960@kpnmail.nl>
2017-11-16 16:07:37 +0100

    spelers op  volgorde

--- spelers.php
+++ spelers.php
@@ -7,14 +7,41 @@ $password = '';
 $conn = new mysqli($hostname, $username, $password, $databasenaam);
 if (isset($_GET['rugnummer'])) {
     $sql = "INSERT INTO `spelers`(`rugnummer`, `naam`)VALUES('" . $_GET['rugnummer'] . "','" . $_GET['naam'] . "')";
-    echo $sql;
+    
     $conn->query($sql);
 }
 ?>
 
 <html> 
     <head>
+        <link rel = "stylesheet" type = "text/css" href="SportPool.css">       <!--  import van centrale css bestanden -->
+        <link rel = "stylesheet" type = "text/css" href="sportpoolCSS.css"> 
+        <style>
+#spelers {            
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 20%;
+}
 
+#spelers td, #spelers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#spelers tr:nth-child(even){background-color: #f2f2f2;}
+
+#spelers tr:hover {background-color: #ddd;}
+
+#spelers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
+}
+
+        </style>
+        
     </head>
     <body>
         <form action="spelers.php" method="get"  >
@@ -26,5 +53,42 @@ if (isset($_GET['rugnummer'])) {
             <br>
             <input type="submit" value="invoegen">
         </form>
+        
+        
+        
+            <table style="border: 2px" id="spelers" cellspacing="0" cellpadding="0">
+                <div style="overflow-x:auto; padding-top: 200px" style="border: 2px">
+             
+                <?php
+                
+                for ($x = 0; $x <1; $x++) {
+                echo"<th width='40'>";echo"Rugnummer";echo"</th>";
+                echo"<th>";echo"Naam Speler";echo"</th>";
+    
+                
+                
+                $sql = "SELECT `rugnummer`, `naam` FROM `spelers` ORDER BY `rugnummer` ASC;";
+                $result = $conn->query($sql);
+                echo "<tr>";
+                for ($x = 0; $x < $result->num_rows; $x++) {
+                    $row = $result->fetch_assoc();
+                    echo "<tr>";
+                    echo "<td>";
+                    echo $row['rugnummer'];
+                    echo "</td>";
+                    echo "<td>";
+                    echo $row['naam'];
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                }
+                echo "</tr>";
+                ?>  
+            </table>
+        </div>
+        
+        
+        
+        
     </body>
 </html>
